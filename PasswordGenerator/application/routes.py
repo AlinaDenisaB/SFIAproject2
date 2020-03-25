a88ff801bbdf55384bff534e7bf3344110b6cc62
Ubuntu <alina@VMproject.vpxngtjiusvezkkfqse01kcjza.zx.internal.cloudapp.net>
2020-03-25 22:00:31 +0000

    password generator service working

--- PasswordGenerator/application/routes.py
+++ PasswordGenerator/application/routes.py
@@ -5,8 +5,8 @@ import random
 
 @app.route('/passGen', methods=['GET', 'POST'])
 def passGen():
-    specialChar = requests.get('http://specialchar-service:5003/specialChar').text
-    numGen = requests.get('http://letters-service:5001/numGen').text
-    letGen = requests.get('http://numbers-service:5002/letGen').text
+    specialChar = requests.get('http://51.104.244.89:5003/specialChar').text
+    numGen = requests.get('http://51.104.244.89:5001/numGen').text
+    letGen = requests.get('http://51.104.244.89:5002/letGen').text
     var = specialChar+numGen+letGen
     return ''.join(random.sample(var, len(var)))
